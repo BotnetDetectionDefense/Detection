1b01db3dd15bbac5e4d205ab099ca5fc48cd4340
Supriya Somashekar <supriya@Supriyas-MacBook-Pro.local>
2017-11-07 02:56:02 -0800

    Try Except Block Changes

--- Traffic_Capture/TrafficCapture.py
+++ Traffic_Capture/TrafficCapture.py
@@ -4,33 +4,40 @@ import pymysql
 myConnection = pymysql.connect(host ='localhost' ,user='root', password = 'root', database='bot_detect_data')
 cur = myConnection.cursor()
 
-cap = pyshark.FileCapture('packets.pcap')
+cap = pyshark.FileCapture('example.pcap')
 i = 1
+j = 1
 scale = 16
 num_of_bits = 8
 for packet in cap:
     #print("----------- ", i, " -----------")
-    EthSource = str(packet.eth.src_resolved)
-    #print("Eth Source: ", EthSource)
-    EthDestination = str(packet.eth.dst_resolved)
-    #print("Eth Destination", EthDestination)
-    Duration = str(packet.frame_info.time_delta)
-    #print("Duration: ", Duration)
-    Flows = "1"
-    #print("Flows: ", Flows)
     try:
         ServiceList = []
         ServiceList = list(packet.layers)
-        Service =ServiceList[3].layer_name
+        #print("Service List: ", ServiceList[3].layer_name)
+        #print(ServiceList)
+        if ((len(ServiceList)>3) and ServiceList[3].layer_name != "http"):
+            Service = packet.highest_layer
+        else:
+            Service =ServiceList[3].layer_name
     except:
         Service = "Other"
         #print(Service)
-    
-    IPType = packet.eth.type
-    #print(IPType)
+    print(Service)
     try:
+        EthSource = str(packet.eth.src_resolved)
+        #print("Eth Source: ", EthSource)
+        EthDestination = str(packet.eth.dst_resolved)
+        #print("Eth Destination", EthDestination)
+        Duration = str(packet.frame_info.time_delta)
+        #print("Duration: ", Duration)
+        Flows = "1"
+        #print("Flows: ", Flows)
+        IPType = packet.eth.type
+        #print(IPType)
         # IP version 4
         if(IPType == "0x00000800"):
+            #print("IPv4")
             proto = packet.ip.proto
             #print(proto)
             if (proto == "6"):
@@ -38,6 +45,11 @@ for packet in cap:
                 #print(Protocol)
             elif(proto == "17"):
                 Protocol = "UDP"
+            elif(proto == "1"):
+                Protocol = "ICMP"
+                #print(Protocol)
+            else:
+                Protocol = None
                 #print(Protocol)
             #print("Protocol: ", Protocol)
             SourceIP = str(packet.ip.src)
@@ -48,13 +60,15 @@ for packet in cap:
             #print("ToS: ", ToS)
             TotalLength = int(packet.ip.len)
             HeaderLength = int(packet.ip.hdr_len)
+            #print("H Len: ", HeaderLength)
             NoOfBytes = TotalLength - HeaderLength
             Bytes = str(NoOfBytes)
-            print("Bytes: ", Bytes)
+            #print("Bytes: ", Bytes)
             Length = str(packet.length)
             #print("Length: ", Length)
         # IP version 6
         elif(IPType == "0x000086dd"):
+            #print("IPv6")
             proto = packet.ipv6.nxt
             #print(proto)
             if (proto == "6"):
@@ -102,14 +116,16 @@ for packet in cap:
             Flags = "00000000"
             #print("Falgs: ", Flags)
         i = i+1
+        query = "INSERT INTO LiveTraffic (duration, protocol, service, sourceip, sourcemac, sourceport, destinationip, destinationmac, destinationport, flags, tos, packets, bytes) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        #print(query)
+        args = (Duration, Protocol, Service, SourceIP, EthSource, SourcePort, DestinationIP, EthDestination, DestinationPort, Flags, ToS, Length, Bytes)
+        cur.execute(query, args)
+        myConnection.commit()
+        j = j+1
     except:
         #print("Broadcast Packet")
+        i = i+1
         pass
-    
-    query = "INSERT INTO LiveTraffic (duration, protocol, service, sourceip, sourcemac, sourceport, destinationip, destinationmac, destinationport, flags, tos, packets, bytes) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    #print(query)
-    args = (Duration, Protocol, Service, SourceIP, EthSource, SourcePort, DestinationIP, EthDestination, DestinationPort, Flags, ToS, Length, Bytes)
-    cur.execute(query, args)
-    myConnection.commit()
-
 myConnection.close()
+print(i)
+print(j)
