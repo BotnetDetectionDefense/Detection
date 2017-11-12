67dc376880ad168b1c7560259856031df353fa3f
Supriya Somashekar <supriya@Supriyas-MacBook-Pro.local>
2017-11-11 23:06:24 -0800

    Latest

--- CTU13_Detection_Model/Bot_Detection.py
+++ CTU13_Detection_Model/Bot_Detection.py
@@ -1,37 +1,52 @@
 import numpy as np
 import pandas as pd
-#import mysql.connector
-
-#conn = mysql.connector.connect(user='root', password = '1590', database='bot_detect_data')
 import pymysql
 
 conn = pymysql.connect(host ='localhost' ,user='root', password = 'root', database='bot_detect_data')
 #cur = myConnection.cursor()
 
 
-# Importing training data set\
+# Importing traning dataset
 
-X_train = pd.read_sql('SELECT duration, protocol, sourceport, destinationport, tos, packets, bytes, flows FROM CTUTrainData', con=conn)
-Y_train = pd.read_sql('SELECT lables FROM CTUTrainData', con=conn)
+Train_Data=pd.read_sql('SELECT * FROM CTUTrainData', con=conn)
+X_train = Train_Data[['duration', 'protocol', 'sourceport', 'destinationport', 'packets', 'bytes']]
+Y_train = Train_Data[['lables']]
 
 
+# Data pre-processing of the test data
 
-# Importing test dataset
+#Importing the live traffic data
+Test_Data = pd.read_sql('SELECT * FROM LiveTraffic', con=conn)
+i=0
+data = pd.DataFrame(columns=["protocol", "sourceip",'destinationip','sourceport','destinationport','count'])
+#final = pd.DataFrame(columns=["protocol", "sourceip",'destinationip','sourceport','destinationport'], data=[[5,np.nan]])
+x = Test_Data[['protocol','sourceip','destinationip','sourceport','destinationport']]
+for index, row in x.iterrows():
+    count = 0
+    a = row['protocol'], row['sourceip'], row['destinationip'], row['sourceport'], row['destinationport']
+    for index, row in x.iterrows():
+        b = row['protocol'], row['sourceip'], row['destinationip'], row['sourceport'], row['destinationport']
+        if(a==b):
+            count = count + 1
+    data['protocol'].append(row['protocol'])
+    data['sourceip'].append(row['sourceip'])
+    data['destinationip'].append(row['destinationip'])
+    data['sourceport'].append(row['sourceport'])
+    data['destinationport'].append(row['destinationport'])
+    data['count'].append(count)
+    print(a)
+    print("row number: ", i, ", ", " Count: ",  count)
+    i=i+1
 
-X_test=pd.read_sql('SELECT duration, protocol, sourceport, destinationport, tos, packets, bytes, flows FROM LiveTraffic', con=conn)
-Souce_IP_Port = pd.read_sql('SELECT sourceip,sourcemac FROM LiveTraffic', con=conn)
+X_test = 
 X_result = pd.DataFrame()
 X_result = X_test.copy(deep=True)
-#remove trailing spaces in column names
 X_test.rename(columns=lambda x: x.strip(),inplace=True)
 
-
-
 # Label encoding the data
-
 from sklearn.preprocessing import LabelEncoder
 le=LabelEncoder()
-column = ['protocol']
+column = ['protocol','service']
 for col in column:
     if X_test[col].dtypes=='object':
         data=X_train[col].append(X_test[col])
@@ -41,46 +56,38 @@ for col in column:
 
 
 # One hot encoding of the label encoded data
-        
 from sklearn.preprocessing import OneHotEncoder
 enc=OneHotEncoder(sparse=False)
-columns=['protocol']
+columns=['protocol', 'service']
 for col in columns:
-   data=X_train[[col]].append(X_test[[col]])
-   enc.fit(data)
-   Proto = enc.transform(X_train[[col]])
-   Proto=pd.DataFrame(Proto,columns=[(col+"_"+str(i)) for i in data[col]
-        .value_counts().index])
-   Proto=Proto.set_index(X_train.index.values)
-   X_train = X_train.drop(col, axis = 1)
-   X_train=pd.concat([X_train,Proto],axis=1)
-   Proto = enc.transform(X_test[[col]])
-   Proto=pd.DataFrame(Proto,columns=[(col+"_"+str(i)) for i in data[col]
-        .value_counts().index])
-   Proto=Proto.set_index(X_test.index.values)
-   X_test = X_test.drop(col, axis = 1)
-   X_test=pd.concat([X_test,Proto],axis=1)
+    data=X_train[[col]].append(X_test[[col]])
+    enc.fit(data)
+    Proto = enc.transform(X_train[[col]])
+    Proto=pd.DataFrame(Proto,columns=[(col+"_"+str(i)) for i in data[col].value_counts().index])
+    Proto=Proto.set_index(X_train.index.values)
+    X_train = X_train.drop(col, axis = 1)
+    X_train=pd.concat([X_train,Proto],axis=1)
+    Proto = enc.transform(X_test[[col]])
+    Proto=pd.DataFrame(Proto,columns=[(col+"_"+str(i)) for i in data[col].value_counts().index])
+    Proto=Proto.set_index(X_test.index.values)
+    X_test = X_test.drop(col, axis = 1)
+    X_test=pd.concat([X_test,Proto],axis=1)
 
 
-Y_train = (np.ravel(Y_train))
 
 
 # Implementing SVM model to train the model
 
 from sklearn.svm import SVC
+#from sklearn.metrics import accuracy_score
 clf = SVC()
-clf.fit(X_train, Y_train) 
-SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-    decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
-    max_iter=-1, probability=False, random_state=None, shrinking=True,
+clf.fit(X_train, Y_train)
+SVC(C=10000.0, cache_size=100000, class_weight=None, coef0=100.0,
+    decision_function_shape='ovr', degree=3, gamma=1000, kernel='poly',
+    max_iter=-1, probability=True, random_state=1000, shrinking=False,
     tol=0.001, verbose=False)
 predict_out = clf.predict(X_test)
-
-
-# Converting prediction data to a data frame
-
 predict_out = pd.DataFrame(data=predict_out)
-#print(predict_out)
 
 
 # Trackig the bot IPs
@@ -88,19 +95,20 @@ predict_out = pd.DataFrame(data=predict_out)
 Prediction = Souce_IP_Port.join(predict_out)
 Prediction = Prediction.join(X_result)
 Bots = pd.DataFrame()
+Background = pd.DataFrame()
+Legitimate = pd.DataFrame()
 Bots = Prediction[Prediction[0] == 'Botnet\r']
-#print(Prediction[0])
-Bot_IP = Bots.drop_duplicates(subset=None, keep='first', inplace=False)
-#print(Bot_IP['sourceip'])
+Background = predict_out[predict_out[0] == 'Background\r']
+Legitimate = predict_out[predict_out[0] == 'LEGITIMATE\r']
+#print (accuracy_score(Y_test, predict_out))
 
 
-for index, row in Bot_IP.iterrows():
+#Insert the the bot data into traning table
+
+for index, row in Bots.iterrows():
     cur = conn.cursor()
     query = "INSERT INTO CTUTrainData (duration, protocol, sourceport, destinationport, tos, packets, bytes, flows, lables) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     args = (row['duration'], row['protocol'],row['sourceport'], row['destinationport'],row['tos'],row['packets'],row['bytes'],row['flows'],row[0])
     cur.execute(query, args)
     conn.commit()
-    
-writer = pd.ExcelWriter('output.xlsx')
-Bot_IP.to_excel(writer,'Sheet1')
-writer.save()
+
--- Traffic_Capture/TrafficCapture.py
+++ Traffic_Capture/TrafficCapture.py
@@ -1,131 +1,108 @@
 import pyshark
 import pymysql
 
+#### Connection to MySQL Database #####
 myConnection = pymysql.connect(host ='localhost' ,user='root', password = 'root', database='bot_detect_data')
 cur = myConnection.cursor()
 
 cap = pyshark.FileCapture('example.pcap')
-i = 1
-j = 1
+
 scale = 16
 num_of_bits = 8
 for packet in cap:
-    #print("----------- ", i, " -----------")
+    ##### Service #####
     try:
         ServiceList = []
         ServiceList = list(packet.layers)
-        #print("Service List: ", ServiceList[3].layer_name)
-        #print(ServiceList)
         if ((len(ServiceList)>3) and ServiceList[3].layer_name != "http"):
             Service = packet.highest_layer
         else:
             Service =ServiceList[3].layer_name
     except:
         Service = "Other"
-        #print(Service)
-    print(Service)
     try:
+        ##### Source and Destination MAC Address #####
         EthSource = str(packet.eth.src_resolved)
-        #print("Eth Source: ", EthSource)
         EthDestination = str(packet.eth.dst_resolved)
-        #print("Eth Destination", EthDestination)
-        Duration = str(packet.frame_info.time_delta)
-        #print("Duration: ", Duration)
-        Flows = "1"
-        #print("Flows: ", Flows)
+
+        ##### Start Time of a Packet #####
+        Time = str(packet.frame_info.time_relative)
+
+        ##### IP Version #####
         IPType = packet.eth.type
-        #print(IPType)
-        # IP version 4
+
+        ##### 0x00000800 = IPv4 #####
         if(IPType == "0x00000800"):
-            #print("IPv4")
+            
+            ##### Transport Layer Protocol #####
             proto = packet.ip.proto
-            #print(proto)
             if (proto == "6"):
                 Protocol = "TCP"
-                #print(Protocol)
             elif(proto == "17"):
                 Protocol = "UDP"
             elif(proto == "1"):
                 Protocol = "ICMP"
-                #print(Protocol)
             else:
                 Protocol = None
-                #print(Protocol)
-            #print("Protocol: ", Protocol)
+
+            ##### Source and Destination IP #####
             SourceIP = str(packet.ip.src)
-            #print("Source IP: ", SourceIP)
             DestinationIP = str(packet.ip.dst)
-            #print("Destination IP: ", DestinationIP)
+
+            ##### Type of Service #####
             ToS = str(bin(int(packet.ip.dsfield, scale))[2:].zfill(num_of_bits))
-            #print("ToS: ", ToS)
+
+            ##### Payload Length #####
             TotalLength = int(packet.ip.len)
             HeaderLength = int(packet.ip.hdr_len)
-            #print("H Len: ", HeaderLength)
             NoOfBytes = TotalLength - HeaderLength
             Bytes = str(NoOfBytes)
-            #print("Bytes: ", Bytes)
-            Length = str(packet.length)
-            #print("Length: ", Length)
-        # IP version 6
+
+        ##### 0x000086dd = IPv6 #####
         elif(IPType == "0x000086dd"):
-            #print("IPv6")
+            
+            ##### Transport Layer Protocol #####
             proto = packet.ipv6.nxt
-            #print(proto)
             if (proto == "6"):
                 Protocol = "TCP"
-                #print(Protocol)
             elif(proto == "17"):
                 Protocol = "UDP"
-                #print(Protocol)
             elif(proto == "58"):
                 Protocol = "ICMPv6"
-                #print(Protocol)
             else:
                 Protocol = None
-            #print("Protocol: ", Protocol)
-            SourceIP = str(packet.ipv6.src)
-            #print("Source IP: ", SourceIP)
-            DestinationIP = str(packet.ipv6.dst)
-            #print("Destination IP: ", DestinationIP)
-            ToS = str(bin(int(packet.ipv6.tclass, scale))[2:].zfill(num_of_bits))
-            #print("ToS: ", ToS)
-            Bytes = str(packet.ipv6.plen)
-            #print("Bytes: ", Bytes)
-            Length = str(packet.captured_length)
-            #print("Length: ", Length)
 
+            ##### Source and Destination IP #####
+            SourceIP = str(packet.ipv6.src)
+            DestinationIP = str(packet.ipv6.dst)
+
+            ##### Type Of Service #####
+            ToS = str(bin(int(packet.ipv6.tclass, scale))[2:].zfill(num_of_bits))
+
+            ##### Payload Length #####
+            Bytes = str(packet.ipv6.plen)
+
+        ##### Flag, Source and Destination Protocol #####
         if(proto == "6"):
             SourcePort = str(packet.tcp.srcport)
-            #print("Source Port: ", SourcePort)
             DestinationPort = str(packet.tcp.dstport)
-            #print("Destination Port: ", DestinationPort)
             Flags = str(bin(int(packet.tcp.flags, scale))[2:].zfill(num_of_bits))
-            #print("Falgs: ", Flags)
         elif(proto == "17"):
             SourcePort = str(packet.udp.srcport)
-            #print("Source Port: ", SourcePort)
             DestinationPort = str(packet.udp.dstport)
-            #print("Destination Port: ", DestinationPort)
             Flags = "00000000"
-            #print("Falgs: ", Flags)
         else:
             SourcePort = "7"
-            #print("Source Port: ", SourcePort)
             DestinationPort = "7"
-            #print("Destination Port: ", DestinationPort)
             Flags = "00000000"
-            #print("Falgs: ", Flags)
-        i = i+1
-        query = "INSERT INTO LiveTraffic (duration, protocol, service, sourceip, sourcemac, sourceport, destinationip, destinationmac, destinationport, flags, tos, packets, bytes) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        #print(query)
-        args = (Duration, Protocol, Service, SourceIP, EthSource, SourcePort, DestinationIP, EthDestination, DestinationPort, Flags, ToS, Length, Bytes)
+        #print(Time, Protocol, Service, SourceIP, EthSource, SourcePort, DestinationIP, EthDestination, DestinationPort, Flags, ToS, Bytes)
+        ##### Insert the captured data to the LiveTraffic Table #####
+        query = "INSERT INTO LiveTraffic (captime, protocol, service, sourceip, sourcemac, sourceport, destinationip, destinationmac, destinationport, flags, tos, bytes) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        args = (Time, Protocol, Service, SourceIP, EthSource, SourcePort, DestinationIP, EthDestination, DestinationPort, Flags, ToS, Bytes)
         cur.execute(query, args)
         myConnection.commit()
-        j = j+1
+
     except:
-        #print("Broadcast Packet")
-        i = i+1
         pass
+        
 myConnection.close()
-print(i)
-print(j)
